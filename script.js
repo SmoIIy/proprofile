482318278e5e154c47fd854bc45c128d45873d9e
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2024-03-01 14:14:51 +0100

    Update

--- script.js
+++ script.js
@@ -34,13 +34,34 @@ word.onmouseover = event => {
 
 //Scroll
 
-// const downArrowButton1 = document.querySelector("#downArrowButton1"); 
+let totalScrollDistance = 0;
 
-// downArrowButton1.addEventListener("click", () => {
-//     window.scrollTo({
-//         document.querySelector("#content1"),
-//         behavior: "smooth"
-//     });
-// });
+const downArrowButton1 = document.querySelector("#downArrowButton1");
+
+downArrowButton1.addEventListener("click", () => {
+    const viewportHeight = window.innerHeight; // Get the height of the viewport
+    const scrollIncrement = viewportHeight * 0.7; // Set scroll increment to 50% of viewport height
+    totalScrollDistance += scrollIncrement; // Increment total scroll distance by the scroll increment
+
+    window.scrollTo({
+        top: totalScrollDistance,
+        behavior: "smooth",
+    });
+});
+
+/* Scroll in Animation */
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+}); 
 
 
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
