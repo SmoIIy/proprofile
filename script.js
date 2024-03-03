1a4027fbd4ae7d554f8ddb2fc8b4367f2594a3c7
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2024-03-03 16:31:18 +0100

    Tilføjet en masse

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ downArrowButton1.addEventListener("click", () => {
     });
 });
 
-/* Scroll in Animation */
+// Scroll in Animation 
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
@@ -65,3 +65,57 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+
+//Box animations
+
+const allLinks = document.querySelectorAll(".tabs a");
+const allTabs = document.querySelectorAll(".tab-content")
+const tabContentWrapper = document.querySelector(".tab-content-wrapper");
+
+const shiftTabs = (linkId) => {
+  allTabs.forEach((tab, i) => {
+      
+    if (tab.id.includes(linkId)) {
+      allTabs.forEach((tabItem) => { 
+        tabItem.style = `transform: translateY(-${i*575}px);`;
+      });
+    }
+  });
+}
+
+allLinks.forEach((elem) => {
+  elem.addEventListener('click', function() {
+    const linkId = elem.id;
+    const hrefLinkClick = elem.href;
+
+    allLinks.forEach((link, i) => {
+      if (link.href == hrefLinkClick){
+        link.classList.add("active");
+      } else {
+        link.classList.remove('active');
+      }
+    });
+
+    shiftTabs(linkId);
+  });
+});
+
+// handle proper selection for initial load
+const currentHash = window.location.hash;
+
+let activeLink = document.querySelector(`.tabs a`);
+
+if (currentHash) {
+  const visibleHash = document.getElementById(
+    `${currentHash.replace('#', '')}`
+  );
+
+  if (visibleHash) {
+    activeLink = visibleHash;
+  }
+}
+
+activeLink.classList.toggle('active');
+
+shiftTabs(activeLink.id);
